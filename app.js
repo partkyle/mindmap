ae70a0ab3a7d9a4f444b1ad5845430a8df6e525f
Kyle Partridge <partkyle@gmail.com>
2011-08-23 13:45:54 -0700

    converted app to use less

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
